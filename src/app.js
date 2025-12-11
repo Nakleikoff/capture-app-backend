7d2c6cf40fd46a4fc77b9f53dc7b3c77ed600911
Saxon Moore <saxon.moore@mtn.com>
2025-12-11 16:13:50 +0200

    Add routes, controller and model for teammates

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import helmet  from 'helmet';
-import {cors} from './middleware/cors.js';
-
+import helmet from 'helmet';
+import { cors } from './middleware/cors.js';
+import { connectDB } from './config/database.js';
+import teammateRoutes from './routes/teammateRoutes.js';
 
 dotenv.config();
 
 const app = express();
 
+connectDB();
+
 app.use(helmet());
 app.use(
-    cors([
-        "http://localhost:3000",
-        "https://capture-app-frontend.vercel.app" //adjust once we deploy
-    ])
-)
+  cors([
+    'http://localhost:3000',
+    'https://capture-app-frontend.vercel.app' //adjust once we deploy
+  ])
+);
 
-export {app};
+app.use('/api/teammates', teammateRoutes);
+
+export { app };
