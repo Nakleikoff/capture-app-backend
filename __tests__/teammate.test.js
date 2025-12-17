87784ecfaa719b41243e707d374e118f8523a748
Alexey Maleykov <alexey.maleykov@entelect.co.za>
2025-12-17 13:10:32 +0200

    Remove duplicating export

--- __tests__/teammate.test.js
+++ __tests__/teammate.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from '../src/app.js';
+import { app } from '../src/app.js';
 import Teammate from '../src/models/teammate.js';
 import { jest } from "@jest/globals";
 
