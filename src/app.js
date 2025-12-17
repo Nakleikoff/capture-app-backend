048315b595e4e615031c32961a58c25af782da2c
Saxon Moore <saxon.moore@mtn.com>
2025-12-17 16:10:51 +0200

    Fix request body destructuring error

--- src/app.js
+++ src/app.js
@@ -12,12 +12,15 @@ const app = express();
 connectDB();
 
 app.use(helmet());
+
 app.use(
   cors([
     'http://localhost:3000',
-    'https://capture-app-frontend.vercel.app', //adjust once we deploy
-  ]),
+    'https://capture-app-frontend.vercel.app' //adjust once we deploy
+  ])
 );
+
+app.use(express.json());
 
 app.use('/api/teammates', teammateRoutes);
 
